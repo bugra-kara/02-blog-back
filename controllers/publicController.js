86fe2313ccd492b4d7aeed5c9d5689677301e906
bugra <sadeceabk@gmail.com>
2023-01-28 20:21:23 +0300

    last update

--- controllers/publicController.js
+++ controllers/publicController.js
@@ -5,16 +5,16 @@ const getPosts = async (req,res) => {
     try {
         const headline = await queries(`SELECT post_uid, status, fullName, title, slug, categories, tags, created_date, image_url 
         FROM (SELECT CONCAT(first_name, ' ', last_name) as fullName, id FROM users) as sub 
-        INNER JOIN posts ON author = sub.id WHERE status = '1' AND is_headline = 'true' AND 'Öne Çıkanlar' = ANY(posts.categories) ORDER BY created_date DESC LIMIT 5`)
+        INNER JOIN posts ON author = sub.id WHERE status = '1' AND is_headline = 'true' AND 'One Cikanlar' = ANY(posts.categories) ORDER BY created_date DESC LIMIT 5`)
         const lastNews = await queries(`SELECT post_uid, status, fullName, title, slug, categories, tags, created_date, image_url 
         FROM (SELECT CONCAT(first_name, ' ', last_name) as fullName, id FROM users) as sub 
-        INNER JOIN posts ON author = sub.id WHERE status = '1' AND is_headline = 'false' AND 'Öne Çıkanlar' = ANY(posts.categories) ORDER BY created_date DESC`)
+        INNER JOIN posts ON author = sub.id WHERE status = '1' AND is_headline = 'false' AND 'One Cikanlar' = ANY(posts.categories) ORDER BY created_date DESC`)
         const bitcoin = await queries(`SELECT post_uid, status, fullName, title, slug, categories, tags, created_date, image_url 
         FROM (SELECT CONCAT(first_name, ' ', last_name) as fullName, id FROM users) as sub 
         INNER JOIN posts ON author = sub.id WHERE status = '1' AND 'Bitcoin' = ANY(posts.categories) ORDER BY created_date DESC`)
         const otherNews = await queries(`SELECT post_uid, status, fullName, title, slug, categories, tags, created_date, image_url 
         FROM (SELECT CONCAT(first_name, ' ', last_name) as fullName, id FROM users) as sub 
-        INNER JOIN posts ON author = sub.id WHERE status = '1' AND 'Öne Çıkanlar' != ANY(posts.categories) AND is_headline = 'false' ORDER BY created_date DESC`)
+        INNER JOIN posts ON author = sub.id WHERE status = '1' AND 'One Cikanlar' != ANY(posts.categories) AND is_headline = 'false' ORDER BY created_date DESC`)
         res.json({result: "success", data: {headline: headline.rows, lastNews: lastNews.rows, bitcoin: bitcoin.rows, otherNews: otherNews.rows}})
     } catch (error) {
         res.json({result: "failed", msg: error})
