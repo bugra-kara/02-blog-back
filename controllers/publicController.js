9d9b69096e19f9f6d0830bc4474c60f623b07047
bugra <sadeceabk@gmail.com>
2023-01-30 19:28:13 +0300

    bitcoin&lastnewsadd

--- controllers/publicController.js
+++ controllers/publicController.js
@@ -6,7 +6,8 @@ const getPosts = async (req,res) => {
         const headline = await queries(`SELECT post_uid, status, fullName, title, slug, categories, tags, created_date, image_url 
         FROM (SELECT CONCAT(first_name, ' ', last_name) as fullName, id FROM users) as sub 
         INNER JOIN posts ON author = sub.id WHERE status = '1' AND is_headline = 'true' AND 'One Cikanlar' = ANY(posts.categories) ORDER BY created_date DESC LIMIT 5`)
-        const lastNews = await queries(`SELECT post_uid, status, fullName, title, slug, categories, tags, created_date, image_url 
+        if(req.query === 'main') {
+            const lastNews = await queries(`SELECT post_uid, status, fullName, title, slug, categories, tags, created_date, image_url 
         FROM (SELECT CONCAT(first_name, ' ', last_name) as fullName, id FROM users) as sub 
         INNER JOIN posts ON author = sub.id WHERE status = '1' AND is_headline = 'false' AND 'One Cikanlar' = ANY(posts.categories) ORDER BY created_date DESC LIMIT 5`)
         const bitcoin = await queries(`SELECT post_uid, status, fullName, title, slug, categories, tags, created_date, image_url 
@@ -15,8 +16,9 @@ const getPosts = async (req,res) => {
         const otherNews = await queries(`SELECT post_uid, status, fullName, title, slug, categories, tags, created_date, image_url 
         FROM (SELECT CONCAT(first_name, ' ', last_name) as fullName, id FROM users) as sub 
         INNER JOIN posts ON author = sub.id WHERE status = '1' AND 'One Cikanlar' != ANY(posts.categories) AND is_headline = 'false' ORDER BY created_date DESC LIMIT 5`)
-        
         res.json({result: "success", data: {headline: headline.rows, lastNews: lastNews.rows, bitcoin: bitcoin.rows, otherNews: otherNews.rows}})
+    }
+    res.json({result: "success", data: {headline: headline.rows}})
     } catch (error) {
         res.json({result: "failed", msg: error})
     }
